3bfe0a649c90c3350b10f0df3316e64d4edd0172
Audry Wolters <audrywolters@gmail.com>
2022-04-08 11:45:11 -0500

    more comments and css.

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -5,6 +5,7 @@ import Spacer from '../components/Spacer'
 import { Context as AuthContext } from '../context/AuthContext'
 
 const SignupScreen = ({ navigation }) => {
+	// get App state and Auth signup function
 	const { state, signup } = useContext(AuthContext)
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
@@ -14,22 +15,34 @@ const SignupScreen = ({ navigation }) => {
 			<Spacer>
 				<Text h3>Sign Up for Tracker</Text>
 			</Spacer>
-			<Input
-				label="Email"
-				value={email}
-				onChangeText={setEmail}
-				autoCapitalize="none"
-				autoCorrect={false}
-			/>
-			<Spacer />
-			<Input
-				secureTextEntry
-				label="Password"
-				value={password}
-				onChangeText={setPassword}
-				autoCapitalize="none"
-				autoCorrect={false}
-			/>
+
+			<Spacer>
+				{/* using rn-elements here to add a label w/o writing it out */}
+				{/* value is the state */}
+				{/* onChangeText is the setState */}
+				<Input
+					label="Email"
+					value={email}
+					onChangeText={setEmail}
+					// below is longhand 
+					// onChangeText={(newEmail) => setEmail(newEmail)}
+					autoCapitalize="none"
+					autoCorrect={false}
+				/>
+			</Spacer>
+
+			<Spacer>
+				<Input
+					secureTextEntry // hide the password text
+					label="Password"
+					value={password}
+					onChangeText={setPassword}
+					autoCapitalize="none"
+					autoCorrect={false}
+				/>
+			</Spacer>
+
+			{/* signup comes from AuthContext */}
 			<Spacer>
 				<Button title="Sign Up" onPress={() => signup({ email, password })} />
 			</Spacer>
@@ -39,6 +52,11 @@ const SignupScreen = ({ navigation }) => {
 
 SignupScreen.navigationOptions = () => {
 	return {
+		// hide the topmost bit, headery piece
+			// can be written as assigning an object
+			// SignupScreen.navigationOptions = {
+			// 	headerShown: false
+			// }
 		header: () => false
 	}
 }
@@ -47,7 +65,7 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		justifyContent: 'center',
-		marginBottom: 250
+		marginBottom: 200
 	}
 })
 
